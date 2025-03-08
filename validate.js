807b0aabd88c4cb9a624d36e3c217ae27d388faa
J <jjrock454@gmail.com>
2025-03-09 01:06:04 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -1,34 +1,35 @@
 function checkSID() {
-    let sid = (document.getElementById("sid").value).trim();
-    if (sid.length === 10 && /^\d{10}$/.test(sid)) {
-      return true;
-    } else {
-      return false;
-    }
-}
- 
+	let sid = (document.getElementById("sid").value).trim();
+	if (sid.length === 10 && /^\d{10}$/.test(sid)) {
+	  return true;
+	} else {
+	  return false;
+	}
+  }
+   
   function checkCandiNo() {
-    let candi = (document.getElementById("candi").value).trim();
-    if (/^\d+$/.test(candi) && parseInt(candi, 10) > 0) {
-      return true;
-    } else {
-      return false;
-    }
-}
- 
+	let candi = (document.getElementById("candi").value).trim();
+	let candiNum = parseInt(candi, 10);
+	if (/^\d+$/.test(candi) && candiNum >= 1 && candiNum <= 10) {
+	  return true;
+	} else {
+	  return false;
+	}
+  }
+   
   function validateForm(){
-    if(!checkSID()){
-      alert("Invalid value for Student ID! It must be a 10-digit number.");
-      document.getElementById("sid").focus();
-      return false;
-} else {
-    if(!checkCandiNo()){
-        alert("Invalid value for Candidate No! It must be a positive number.");
-        document.getElementById("candi").focus();
-        return false;
-    } else {
-        alert("Your input data passes validation!!");
-        return true;
-    }
-}
-}
+	if(!checkSID()){
+	  alert("Invalid value for Student ID! It must be a 10-digit number.");
+	  document.getElementById("sid").focus();
+	  return false;
+	} else {
+	  if(!checkCandiNo()){
+		alert("Invalid value for Candidate No! It must be a number between 1 and 10.");
+		document.getElementById("candi").focus();
+		return false;
+	  } else {
+		alert("Your input data passes validation!!");
+		return true;
+	  }
+	}
+  }
